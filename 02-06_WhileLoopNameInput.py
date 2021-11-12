0f61080bd49b9f4fe885d1b0a5bb2383c022c181
triobot <daniel.kun.tin.ho@gmail.com>
2021-11-12 18:27:32 +0000

    Changing 1st condition to while True. Once your name has been entered condition meet, breaks loop, continues.

--- 02-06_WhileLoopNameInput.py
+++ 02-06_WhileLoopNameInput.py
@@ -1,6 +1,8 @@
 name = ''
-while name !=  'your name':
+while True:
     print('Please type your name.')
     name = input()
+    if name == 'your name':
+        break    
 print('Thank You!')
 # Until you type ''your name'' as the input, the program will loop until ''your name'' is entered.
