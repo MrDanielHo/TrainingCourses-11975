50db5f8849f6446c96aa275fd1e0094bfdfcc2e0
triobot <daniel.kun.tin.ho@gmail.com>
2021-11-12 18:24:17 +0000

    Learning While Loops

---
+++ 02-06_FirstWhileLoop.py
@@ -0,0 +1,7 @@
+spam = 0
+while spam < 5:
+    print ('Hello world!')
+    spam = spam + 1
+# This ''while'' loop iterates until the value of spam is greater than five.
+# loops on cycle spam = [0 - 4].
+# when spam is 5 the program ends.
---
+++ 02-06_WhileLoopNameInput.py
@@ -0,0 +1,6 @@
+name = ''
+while name !=  'your name':
+    print('Please type your name.')
+    name = input()
+print('Thank You!')
+# Until you type ''your name'' as the input, the program will loop until ''your name'' is entered.
