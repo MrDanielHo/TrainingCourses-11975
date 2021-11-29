d9f5852061c833bbec4a16ca3601a93753acf386
triobot <daniel.kun.tin.ho@gmail.com>
2021-11-29 11:17:06 +0000

    Install third part modules

---
+++ 03-08_Built-InFunctions.py
@@ -0,0 +1,8 @@
+# import random
+# print(random.randint(1,10))
+# pip3 install ''Module''
+import random, sys, os, math, pyperclip
+from random import *
+pyperclip.copy(str(randint(1,10)))
+
+print(pyperclip.paste)
