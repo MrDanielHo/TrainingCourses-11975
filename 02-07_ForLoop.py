35add017df9515f42094feaecf64a315b9dd3147
triobot <daniel.kun.tin.ho@gmail.com>
2021-11-29 11:00:54 +0000

    Section 2.7 For Loops

---
+++ 02-07_ForLoop.py
@@ -0,0 +1,28 @@
+# print('My name is')
+# for i in range(5):
+#     print('Jimmy Five Times ' + str(i))
+
+# print('My name is')
+# i = 0
+# while i < 5:
+#     print('Jimmy Five Times ' + str(i))
+#     i += 1
+
+'''
+Carl Friedrich Gauss' solution to add the totals of numbers between 0-100
+'''
+# total = 0
+# for num in range(101):
+#     total = total + num
+# print(total)
+'''
+The above for loop solves this problem.
+How Gauss solved it was to add the pairs of numbers that add up to 100:
+01+99...
+02+98...
+49+51
+= 5000
++50
+=5050
+'''
+
