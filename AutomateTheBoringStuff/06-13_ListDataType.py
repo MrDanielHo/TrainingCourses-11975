d32676fca4700dacfd15eb1428869cf9c6324b22
triobot <daniel.kun.tin.ho@gmail.com>
2021-12-05 01:06:45 +0000

    Data Type: Lists and how they work.

---
+++ AutomateTheBoringStuff/06-13_ListDataType.py
@@ -0,0 +1,37 @@
+# spam = ['cat', 'bat', 'rat', 'elephant']
+# print(spam)
+# print(spam[0])
+# print(f"The {spam[-1]} is scared of the {spam[2]}")
+
+
+# ham = [['car','truck'],[10,20,30,40,50]]
+# print(ham)
+# print(ham[1][-1])
+# print(ham[1][1:3])
+
+# bacon = [10,20,30]
+# print(bacon)
+# bacon[1] = 'Hello'
+# print(bacon)
+# bacon[1:3] = ['Street','Meat','Cheese']
+# print(bacon)
+
+# eggs = ['mouse','dog','cat','lemur','skunk']
+# print(eggs)
+# print(eggs[:2]) # Slices from first entry [0] to and excluding third entry [2]
+# print(eggs[3:]) # Slices from the fourth entry [3] to the last entry
+# del eggs[3] # Deletes the fourth entry [4] which at this point is lemur
+# print(eggs)
+# del eggs[3] # Deletes the fourth entry [4] which at this point is skunk
+# print(eggs)
+
+# print(len('Five'))
+# print(len([1,2,3,4]))
+
+# print('hello' * 3)
+# print([10,20,30] * 3) # Output is the list 3 times.
+
+# print(list('Pokemon')) # Converts the word into a list.
+
+# print('howdy' in ['hello', 'hi', 'dude', 'howdy', 'alright']) # Output is true as 'howdy' is in the list. Is case sensitive
+# print('Howdy' not in ['hello', 'hi', 'dude', 'howdy', 'alright'])
