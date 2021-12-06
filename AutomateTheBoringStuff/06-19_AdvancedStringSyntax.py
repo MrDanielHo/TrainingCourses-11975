0d3666b022bfcec5f3f6690d53e7b3c3e0be47f5
triobot <daniel.kun.tin.ho@gmail.com>
2021-12-06 09:34:45 +0000

    Some advanced string syntax.

---
+++ AutomateTheBoringStuff/06-19_AdvancedStringSyntax.py
@@ -0,0 +1,19 @@
+'''
+Escape characters:
+\'  Single quote
+\"  Double quote
+\t  Tab
+\n  New line
+\\  Backslash
+'''
+
+print(r'hello\s\n\t') # Raw string
+
+print('''Dear Alice
+
+I write to you because bee bloop blah
+Please write back
+
+Peace
+Wunder
+''')
